3a248054d84bbb696c7ede201df5ef259568be46
ErickYT203 <88186756+ErickYT203@users.noreply.github.com>
2021-10-09 20:01:49 +0530

    a file created as config file
    you know what a config file means?

---
+++ config/bot.js
@@ -0,0 +1,20 @@
+module.exports = {
+    emojis: {
+        off: '<a:v_wrong:894853923914014760>',//the emoji to be shown whenever x is done wrong
+        error: '<a:v_wrong:894853923914014760>',//the emoji to be shown whenever x is having error
+        queue: '<:queue1:894838816051957771>',//the emoji to be shown whenever y is using x command
+        music: '<a:music:894853917794529291>',//the emoji to be shown whenever x is doing some music stuff
+        success: '<a:u_Tick:894853931904159784>',//the emoji to be shown whenever x is done successfully
+    },
+
+    discord: {
+        token: '',//your bot token
+        prefix: '',//your bot default prefix
+        activity: '',//your bot activity
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
+//NOTE: x and y is a variable made for educational purpose only.
+//x = command
+//y = user
